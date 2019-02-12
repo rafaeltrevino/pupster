9aa6333652307f2fc7892fce1205c3260c2b8006
trevino <rafael.trevino@me.com>
2019-02-12 13:08:47 -0500

    initial files

---
+++ pupster/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "https://dog.ceo/api/breeds/image/random";
+const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
+// Export an object with a "search" method that searches the Giphy API for the passed query
+export default {
+  search: function(query) {
+    return axios.get(BASEURL + query + APIKEY);
+  }
+};
